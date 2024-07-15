75bb3e898453b5be3fee9ea76b1e703660dcdc34
banner4422 <68676614+banner4422@users.noreply.github.com>
2024-07-15 23:00:03 +0200

    feat(navbar): client load dark mode button only, deprecated imports update

--- src/components/general/navigation/NavigationBar.tsx
+++ src/components/general/navigation/NavigationBar.tsx
@@ -1,6 +1,15 @@
 "use client";
 import { Fragment, useEffect, useState } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import {
+	Disclosure,
+	DisclosureButton,
+	DisclosurePanel,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuItems,
+	Transition,
+} from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon, MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
@@ -24,7 +33,7 @@ export interface navigationRoute {
 }
 
 export interface navigationExtendedRoute extends navigationRoute {
-	// på den her måde kan vi tilføje flere routes til en dropdown/popover menu
+	// in case we want to add a dropdown menu
 	group?: navigationRoute[];
 }
 
@@ -57,8 +66,6 @@ export default function NavigationBar({ navigationRoutes, notifications, avatar 
 		return () => window.removeEventListener("scroll", toggleVisibility);
 	}, []);
 
-	if (!loaded) return null;
-
 	return (
 		<Disclosure
 			as="nav"
@@ -72,14 +79,14 @@ export default function NavigationBar({ navigationRoutes, notifications, avatar 
 						<div className="relative flex h-16 items-center justify-between">
 							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
 								{/* Mobile menu button*/}
-								<Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 dark:text-white text-black dark:hover:bg-yellow-500 hover:bg-yellow-400 hover:text-black ">
+								<DisclosureButton className="inline-flex items-center justify-center rounded-md p-2 dark:text-white text-black dark:hover:bg-yellow-500 hover:bg-yellow-400 hover:text-black ">
 									<span className="sr-only">Open main menu</span>
 									{open ? (
 										<XMarkIcon className="block h-6 w-6" aria-hidden="true" />
 									) : (
 										<Bars3Icon className="block h-6 w-6" aria-hidden="true" />
 									)}
-								</Disclosure.Button>
+								</DisclosureButton>
 							</div>
 							<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
 								<Link
@@ -145,10 +152,10 @@ export default function NavigationBar({ navigationRoutes, notifications, avatar 
 								{avatar && (
 									<Menu as="div" className="relative ml-3">
 										<div>
-											<Menu.Button className="flex rounded-full bg-zinc-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-800">
+											<MenuButton className="flex rounded-full bg-zinc-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-800">
 												<span className="sr-only">Open user menu</span>
 												<Image className="h-8 w-8 rounded-full" src="/29.png" width={10} height={10} alt="" />
-											</Menu.Button>
+											</MenuButton>
 										</div>
 										<Transition
 											as={Fragment}
@@ -159,8 +166,8 @@ export default function NavigationBar({ navigationRoutes, notifications, avatar 
 											leaveFrom="transform opacity-100 scale-100"
 											leaveTo="transform opacity-0 scale-95"
 										>
-											<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none">
-												<Menu.Item>
+											<MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none">
+												<MenuItem>
 													{({ active }) => (
 														<Link
 															href="#"
@@ -172,8 +179,8 @@ export default function NavigationBar({ navigationRoutes, notifications, avatar 
 															Your Profile
 														</Link>
 													)}
-												</Menu.Item>
-												<Menu.Item>
+												</MenuItem>
+												<MenuItem>
 													{({ active }) => (
 														<Link
 															href="#"
@@ -185,8 +192,8 @@ export default function NavigationBar({ navigationRoutes, notifications, avatar 
 															Settings
 														</Link>
 													)}
-												</Menu.Item>
-												<Menu.Item>
+												</MenuItem>
+												<MenuItem>
 													{({ active }) => (
 														<Link
 															href="#"
@@ -198,34 +205,34 @@ export default function NavigationBar({ navigationRoutes, notifications, avatar 
 															Sign out
 														</Link>
 													)}
-												</Menu.Item>
-											</Menu.Items>
+												</MenuItem>
+											</MenuItems>
 										</Transition>
 									</Menu>
 								)}
 							</div>
 						</div>
 					</div>
-					<Disclosure.Panel className="sm:hidden">
+					<DisclosurePanel className="sm:hidden">
 						<div className="space-y-1 px-2 pb-3 pt-2">
 							{navigationRoutes.map((item) => (
-								<Disclosure.Button
+								<DisclosureButton
 									key={item.name}
 									as="a"
 									href={item.href}
 									className={classNames(
 										item.current
-											? "dark:bg-yellow-500 bg-yellow-400 dark:text-zinc-100 text-white dark:text-black"
+											? "dark:bg-yellow-500 bg-yellow-400 text-white dark:text-black"
 											: "dark:text-white text-black dark:hover:bg-yellow-500 hover:bg-yellow-400 hover:text-black",
 										"block rounded-md px-3 py-2 text-base font-medium transition-colors duration-300 ease-in-out text-center",
 									)}
 									aria-current={item.current ? "page" : undefined}
 								>
 									{item.name}
-								</Disclosure.Button>
+								</DisclosureButton>
 							))}
 						</div>
-					</Disclosure.Panel>
+					</DisclosurePanel>
 				</Fragment>
 			)}
 		</Disclosure>
