7a0aa52fbc03687101121b65434edee6fe9d04e9
banner4422 <christian@chrkn.dev>
2024-08-19 15:00:44 +0200

    feat(markdown pages): add loading skeleton

---
+++ src/components/general/GithubMarkdownSkeleton.tsx
@@ -0,0 +1,19 @@
+export default function SkeletonMarkdown() {
+	return (
+		<div className="animate-pulse space-y-14 mx-auto max-w-3xl p-4">
+			<div className="h-8 dark:bg-zinc-700 bg-zinc-300 rounded w-2/5 mb-6"></div>
+			<div className="space-y-12">
+				<div className="h-4 dark:bg-zinc-700 bg-zinc-300 rounded w-full"></div>
+				<div className="h-4 dark:bg-zinc-700 bg-zinc-300 rounded w-5/6"></div>
+				<div className="h-4 dark:bg-zinc-700 bg-zinc-300 rounded w-4/6"></div>
+				<div className="h-4 dark:bg-zinc-700 bg-zinc-300 rounded w-3/4"></div>
+				<div className="h-4 dark:bg-zinc-700 bg-zinc-300 rounded w-full"></div>
+				<div className="h-4 dark:bg-zinc-700 bg-zinc-300 rounded w-5/6"></div>
+				<div className="h-4 dark:bg-zinc-700 bg-zinc-300 rounded w-4/6"></div>
+				<div className="h-4 dark:bg-zinc-700 bg-zinc-300 rounded w-3/4"></div>
+				<div className="h-4 dark:bg-zinc-700 bg-zinc-300 rounded w-full"></div>
+				<div className="h-4 dark:bg-zinc-700 bg-zinc-300 rounded w-5/6"></div>
+			</div>
+		</div>
+	);
+}
