f1fd2f2c5086268189aec65359fc052f5d6332f4
banner4422 <christian@chrkn.dev>
2024-11-06 11:27:37 +0100

    fix(leaderboard/[id]): upgrade to async params, new next 15 feat

--- src/app/leaderboard/[id]/page.tsx
+++ src/app/leaderboard/[id]/page.tsx
@@ -6,13 +6,18 @@ import { LeaderboardPageSkeleton } from "@/components/leaderboard/LeaderboardPag
 
 export const revalidate = 3600; // 1 hour
 
-export default function Page({ params }: { params: { id: string } }) {
+type Params = Promise<{ id: string }>;
+
+export default async function Page(props: { params: Params }) {
+	const params = await props.params;
+	const { id } = params;
+
 	return (
 		<SectionWrapper>
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
 				<div className="lg:text-center text-center overflow-hidden">
-					<Suspense fallback={<LeaderboardPageSkeleton serverId={params.id} />}>
-						<LeaderboardAsync serverId={params.id} />
+					<Suspense fallback={<LeaderboardPageSkeleton serverId={id} />}>
+						<LeaderboardAsync serverId={id} />
 					</Suspense>
 				</div>
 			</div>
